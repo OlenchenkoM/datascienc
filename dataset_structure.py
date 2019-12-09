131e80bf11396fd86de65e96c1bef98657c13dfa
Maksym Olenchenko <55354656+OlenchenkoM@users.noreply.github.com>
2019-12-09 19:07:48 +0200

    pull

---
+++ dataset_structure.py
@@ -0,0 +1,22 @@
+dataset = {
+    "AVpe__eOilAPnD_xSt-H  ": {
+    "weight : 1 kg " 
+    "name Handcrafted Alpaca Blend 'Purple Charisma ' Sweater (Peru) "
+    "brand - Novica "
+    "dimension - 6.0 in x 6.0 in x 1.0 in 2"
+    },
+    "AVpe_0ob1cnluZ0-bnWr": {
+        "weight : 1 kg"
+        "name Sof Comfort Women's : Everyday Gel Insoles, 2-pair"
+        "brand BEAUTIFEET"
+        "dimension 5.0 in x 4.0 in x 0.5 in 1"
+    },
+    "AVpe_-0YilAPnD_xStvJ": {
+        "weight 2 kg"
+        "name Marc Fisher Fawna Women Cap Toe Leather Oxford"
+        "brand Marc Fisher "
+        "dimension 6.0 in x 7.0 in x 1 in 1"
+    }
+
+
+}
